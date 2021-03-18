555ab6dd2f4ab1f3c3ee5ccc319826b9a78374b5
Emile Paffard-Wray <emilepw93@gmail.com>
2021-03-18 21:39:53 +0000

    Add user submission timeline

---
+++ src/components/SubmissionTimeline.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { monthsOfTheYear } from '../lib/dateUtils'
+  export let submissions
+</script>
+
+<div class="flex flex-col w-full my-12">
+  {#each submissions as { title, date }, i}
+    <div class="flex flex-row">
+      <div
+        class="w-full md:w-1/2 border-bluegray-600 {i % 2 ? 'md:order-first md:border-r-2 md:pr-9' : 'md:order-last md:border-l-2 md:pl-9'}
+        md:relative pt-0 pb-12 flex flex-col items-center md:items-start">
+        <div
+          class="flex flex-col items-center justify-center p-1 font-semibold
+          rounded-full {i % 2 ? 'md:-right-7' : 'md:-left-7'} md:-top-7
+          md:absolute w-14 h-14 bg-bluegray-500 text-bluegray-800">
+          <span>{date.getDate()}</span>
+          <span class="text-sm">
+            {monthsOfTheYear[date.getMonth()].slice(0, 3)}
+          </span>
+        </div>
+        <div class="w-full pt-6 md:border-t-2 border-bluegray-600">
+          <h4 class="mt-4 text-sm text-bluegray-800">Completed a step</h4>
+          <h3 class="mb-4 font-bold">{title}</h3>
+          <div class="w-full h-32 bg-bluegray-600" />
+        </div>
+      </div>
+      <div class="hidden md:block md:w-1/2" />
+    </div>
+  {/each}
+</div>
--- src/components/UserProfile.svelte
+++ src/components/UserProfile.svelte
@@ -1,30 +1,45 @@
 <script>
-  import UserOverview from "./UserOverview.svelte";
-  import UserSteps from "./UserSteps.svelte";
-  import UserMetrics from "./UserMetrics.svelte";
+  import UserOverview from './UserOverview.svelte'
+  import UserSteps from './UserSteps.svelte'
+  import UserMetrics from './UserMetrics.svelte'
+  import SubmissionTimeline from './SubmissionTimeline.svelte'
 
   export let user
   export let lockedBehaviors
   export let steps
-</script>
 
-<div class="user-profile-outer-container">
-  {#if user}
-    <div class="flex flex-col items-center w-full px-6 user-profile-content-container">
-      <UserOverview {user} {lockedBehaviors} />
-      <UserSteps {user} {steps} />
-      <UserMetrics />
-    </div>
-  {/if}
-</div>
+  const submissions = Object.entries(user.submissionsByStep).map(
+    ([stepId, submission]) => {
+      const step = steps[stepId]
+      return {
+        title: step.title,
+        date: new Date(submission[0].createdAt),
+      }
+    },
+  ).sort((a, b) => {
+    return new Date(b.date).getTime() - new Date(a.date).getTime() // Sort in reverse chronological order
+  })
+</script>
 
 <style>
   .user-profile-outer-container {
-    background: linear-gradient(#F2F4F7 0%, #FFFFFF 100%);
+    background: linear-gradient(#fbfcfd 0%, #ffffff 100%);
   }
-  
+
   .user-profile-content-container {
     margin: 0 auto;
     max-width: 910px;
   }
 </style>
+
+<div class="py-4 user-profile-outer-container">
+  {#if user}
+    <div
+      class="flex flex-col items-center w-full px-6 user-profile-content-container">
+      <UserOverview {user} {lockedBehaviors} />
+      <UserSteps {user} {steps} />
+      <UserMetrics />
+      <SubmissionTimeline {submissions} />
+    </div>
+  {/if}
+</div>
