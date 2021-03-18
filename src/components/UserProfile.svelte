56e0a05867c2bfaf04255c23185a3b5ac4491572
Emile Paffard-Wray <emilepw93@gmail.com>
2021-03-18 17:16:58 +0000

    Add progress bar w/ user's % complete

--- src/components/UserProfile.svelte
+++ src/components/UserProfile.svelte
@@ -1,12 +1,21 @@
 <script>
   import UserOverview from "./UserOverview.svelte";
+  import UserSteps from "./UserSteps.svelte";
 
   export let user
   export let lockedBehaviors
 </script>
 
 {#if user}
-  <div class="flex flex-col items-center w-full">
+  <div class="flex flex-col items-center w-full px-6 user-profile-container">
     <UserOverview {user} {lockedBehaviors} />
+    <UserSteps {user} />
   </div>
 {/if}
+
+<style>
+  .user-profile-container {
+    margin: 0 auto;
+    max-width: 910px;
+  }
+</style>
---
+++ src/components/UserSteps.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Avatar from './Avatar.svelte'
+  export let user
+</script>
+
+<div class="flex items-end w-full my-4 flex-column">
+  <div class="relative w-full h-10 bg-bluegray-600">
+    <div
+      class="absolute top-0 left-0 z-10 h-full bg-green-500"
+      style="width: {user.percentComplete}%" />
+    <div class="absolute z-10 flex items-center top-1" style="right: {100 - user.percentComplete}%">
+      <Avatar size={2} {user} />
+      <span class="px-2 font-bold text-white">{user.percentComplete}%</span>
+    </div>
+  </div>
+</div>
