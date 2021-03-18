c7113e52d1281995509876d985625cd770e28c95
Emile Paffard-Wray <emilepw93@gmail.com>
2021-03-18 16:27:59 +0000

    Calculate and pass which behaviors are locked for the user
    I have assumed that the unlocked behaviors for each step
    are simply an array of strings and have amended the
    corresponding array in step 2. If the data format was
    different this would need to be adapted.

--- src/routes/user/[id].svelte
+++ src/routes/user/[id].svelte
@@ -10,8 +10,18 @@
   import {board} from '../../lib/board'
 
   export let id
+
+  const lockedBehaviors = Object
+    .entries(board.steps)
+    .map(([stepId, step]) => {
+      const complete = !!board.users[id].stepsCompleted[stepId];
+      if (!complete) {
+        return step.unlockBehaviors.reduce((obj, next) => ({ ...obj, [next]: {  ord: step.ord, title: step.title } }), {})
+      } else return {};
+    })
+    .reduce((obj, next) => Object.assign(obj, next), {})
 </script>
 
 <div class="mt-20">
-  <UserProfile user={board.users[id]} />
+  <UserProfile user={board.users[id]} {lockedBehaviors} />
 </div>
