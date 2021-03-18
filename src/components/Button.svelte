bcff523f372ea8f3e9df97a6bb2b72a0b8d204f3
Emile Paffard-Wray <emilepw93@gmail.com>
2021-03-18 16:19:58 +0000

    Add Button component

---
+++ src/components/Button.svelte
@@ -0,0 +1,7 @@
+<script>
+
+</script>
+
+<button class="flex items-center justify-center h-12 font-semibold text-center uppercase border-2 rounded-lg w-80 md:w-40 border-bluegray-600 text-bluegray-800 ">
+  <slot />
+</button>
